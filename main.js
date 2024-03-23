0a50517f67b33edb04ccff4a54d71c9d24e92f3f
mfahadjbr <mfahadjbr@gmail.com>
2024-03-23 11:16:42 -0700

    added files

---
+++ main.js
@@ -0,0 +1,35 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const mycalculator = await inquirer.prompt([
+    {
+        type: 'list',
+        name: 'cal',
+        message: 'calculator list:',
+        choices: ['add', 'substract', 'multip', 'division', 'modulo']
+    },
+    {
+        type: 'number',
+        name: 'num1',
+        message: 'Enter first number ='
+    },
+    {
+        type: 'number',
+        name: 'num2',
+        message: 'Enter second number ='
+    }
+]);
+if (mycalculator.cal == 'add') {
+    console.log(`Add the two number = ${mycalculator.num1 + mycalculator.num2}`);
+}
+else if (mycalculator.cal == 'substract') {
+    console.log(`substract the two number = ${mycalculator.num1 - mycalculator.num2}`);
+}
+else if (mycalculator.cal == 'multip') {
+    console.log(`multiply the two number = ${mycalculator.num1 * mycalculator.num2}`);
+}
+else if (mycalculator.cal == 'division') {
+    console.log(`division the two number = ${mycalculator.num1 / mycalculator.num2}`);
+}
+else {
+    console.log(`module of two number =`);
+}
